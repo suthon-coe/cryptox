f68afc30d821f721c9fc3dec6701d88721a22b0d
Suthon <suthon@coe.psu.ac.th>
2018-01-12 16:29:24 +0700

    use array of symbols

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 import _ from 'lodash'
 
 class App extends Component {
+  SYMBOLS = ['OMG', 'BTC', 'XRP']
+
   constructor(props){
     super(props)
     this.state = {}
@@ -13,12 +15,11 @@ class App extends Component {
     let self = this
     axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
     .then(function (response) {
-      let btc = _.find(response.data, ['symbol', 'BTC'])
-      let omg = _.find(response.data, ['symbol', 'OMG'])
-      self.setState({
-        BTC: btc.price_thb,
-        OMG: omg.price_thb
+      let price_state = {}
+      _.each(self.SYMBOLS, symbol => {
+        price_state[symbol] = _.find(response.data, ['symbol', symbol]).price_thb
       })
+      self.setState(price_state)
     })
     .catch(function (error) {
       console.log(error);
@@ -28,8 +29,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Ticker symbol="BTC" price={this.state.BTC}/>
-        <Ticker symbol="OMG" price={this.state.OMG}/>
+        {
+          _.map(this.SYMBOLS, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
+        }
       </div>
     );
   }
